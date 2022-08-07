558042769a9cd6de0cf592ab11941b8efe885a3b
vikas <vikasgaur128@gmail.com>
2022-08-08 00:04:42 +0530

    my first react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>home</li>
+      <li> about</li>
+      <li> contact </li>
+    </nav>
+    <div classname="container">
+
+      <p> this is my my first react app  </p>
     </div>
+    
+    </>
   );
 }
 
 export default App;
+  
